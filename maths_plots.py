7b19e2578a1752cf5e8e9bfa0bdfcdcab369ae66
juna21-meet <juna21@meet.mit.edu>
2019-07-28 11:36:25 -0400

    Stopping here

---
+++ maths_plots.py
@@ -0,0 +1,38 @@
+"""import turtle
+x=0
+while x<300:
+    y=x**2/300
+    turtle.goto(x,y)
+    x=x+100
+turtle.mainloop()
+"""
+"""
+import turtle
+num_pts=5
+for i in range (num_pts):
+    turtle.left(360/num_pts)
+    turtle.forward(100)
+turtle.mainloop()
+"""
+
+"""
+result=[]
+for count in range(1,20):
+    if count % 3==0 and count % 5==0:
+        result.append("fizzbuzz")      
+    elif count % 5 ==0:
+        result.append("buzz")
+    elif count % 3==0 and count % 5==0:
+        result.append("fizzbuzz")
+    else:
+        result.append(count)
+    print(result)
+"""
+
+
+
+    
+
+
+
+    
